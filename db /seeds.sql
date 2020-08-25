bdf0ec45ce11949bd216aa370add1395c7aa21f6
Andrea Gamez <andreamgamez@gmail.com>
2020-08-25 11:47:21 -0400

    added folders and functions

---
+++ db /seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('cheese');
+INSERT INTO burgers (burger_name) VALUES ('italian');
+INSERT INTO burgers (burger_name, devoured) VALUES ('avocado', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('sushi', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('beacon', true);
+INSERT INTO burgers (burger_name) VALUES ('tripple deck');

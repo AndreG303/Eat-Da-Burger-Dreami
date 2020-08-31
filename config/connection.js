add7d05655eaea790a3b5cf1f40698fe91f8e4cc
Andrea Gamez <andreamgamez@gmail.com>
2020-08-31 12:50:01 -0400

    connect

--- config/connection.js
+++ config/connection.js
@@ -2,18 +2,19 @@
 var mysql = require("mysql");
 
 var connection = null;
-if(typeof process.env.JAWSDB_URL === "undefined"){
-  connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "mysqlbootcamp",
-  database: "burgers_db"
-});
-}
-else{
+// if(typeof process.env.JAWSDB_URL === "undefined"){
+//   connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "mysqlbootcamp",
+//   database: "burgers_db"
+// });
+// }
+// else{
   connection = mysql.createConnection(process.env.JAWSDB_URL);
-}
+  console.log("connection",connection);
+// }
 // Make connection.
 connection.connect(function(err) {
   if (err) {
